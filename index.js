3c94a8b46d603fc5394bf66212c637143b24cf1e
Isabel <isvaoc@gmail.com>
2021-10-06 19:31:19 -0500

    añadiendo la función que encuentra los links en los archivos md

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 const files = require("./findFiles.js")
+const links = require("./findLinks.js")
 
-let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/pruebas");
-//arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/README.md");
-console.log(arrayFiles)
+//let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/pruebas");
+let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/README.md");
+//let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/pruebas/readme0.md");
+//console.log(arrayFiles)
+
+let arrayLinks = links.findLinks(arrayFiles);
+console.log(arrayLinks)

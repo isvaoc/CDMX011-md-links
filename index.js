730fadb7958181630f4925b7bfc726cf025117b7
Isabel <isvaoc@gmail.com>
2021-10-10 23:33:35 -0500

    añadiendo la función para validar los links de los archivos md

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const fs = require("fs");
 const path = require("path");
 const files = require("./findFiles.js")
 const links = require("./findLinks.js")
+const validateAll = require("./validate.js")
 
 //let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/pruebas");
-let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/README.md");
-//let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/pruebas/readme0.md");
+//let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/README.md");
+let arrayFiles = files.findFiles("C:/Users/ISABEL-PC/Desktop/Laboratoria/CDMX011-md-links/pruebas/readme0.md");
 //console.log(arrayFiles)
 
 let arrayLinks = links.findLinks(arrayFiles);
-console.log(arrayLinks)
+//console.log(arrayLinks)
+//console.log(arrayLinks.length)
+
+ 
+
+validateAll.validateAll(arrayLinks).then(res => {
+    console.log(res)
+    res.forEach(elem => console.log(elem))
+});
+
+
+
